5c9d5884492c52760f561f41f504279704dfbd43
bhavenjain <bj995@snu.edu.in>
2023-01-18 18:55:37 +0530

    comiit

--- users/views.py
+++ users/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 def register(request):
+    if request.user.is_authenticated:
+        return redirect('blog-home')
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
@@ -14,3 +17,7 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form, 'title': 'Register'})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
